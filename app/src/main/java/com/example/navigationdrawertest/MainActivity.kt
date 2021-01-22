5b6998071146a786868837deac8b2fcdb8a1ab99
Anna Naumova <51020604+Anna-Beltanner@users.noreply.github.com>
2021-01-22 15:59:02 +0300

    Initial commit

---
+++ app/src/main/java/com/example/navigationdrawertest/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.example.navigationdrawertest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var  toggle: ActionBarDrawerToggle
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        toggle = ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close)
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        navView.setNavigationItemSelectedListener {
+            when(it.itemId){
+                R.id.item1 -> Toast.makeText(applicationContext,
+                "My profile is clicked", Toast.LENGTH_SHORT).show()
+                R.id.item2 -> Toast.makeText(applicationContext,
+                    "My friends is clicked", Toast.LENGTH_SHORT).show()
+                R.id.item3 -> Toast.makeText(applicationContext,
+                    "My groups is clicked", Toast.LENGTH_SHORT).show()
+            }
+
+            true
+        }
+
+
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(toggle.onOptionsItemSelected(item)){
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
+}
